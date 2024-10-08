a32b9a66d5c0cf5dd1f289d9207afeceec4b9118
amohan179 <157762058+amohan179@users.noreply.github.com>
2024-10-08 21:57:15 +0000

    Update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,14 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <NavBar></NavBar>
     </div>
   );
 }
 function NavBar() {
   return (
     <div>
-      
+      <button>
+        Home
+      </button>
+      <button>
+        Portfolio
+      </button>
+      <button>
+        Autobiography
+      </button>
+      <button>
+        Transcript + Resume
+      </button>
     </div>
   )
 }
